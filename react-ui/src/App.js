67123b27a61ba15d7419f19b04906b06508460ff
Mark renzo Santiago <ms103804@gmail.com>
2017-11-19 23:55:41 -0800

    start new component for result page

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -5,7 +5,7 @@ import Axios from 'axios';
 
 import { Input } from 'semantic-ui-react'
 
-const InputExampleIconProps = () => (
+const SummonerSearchBox = () => (
     <Input
         icon={{ name: 'search', circular: true, link: true }}
         placeholder='Search...'
@@ -13,49 +13,54 @@ const InputExampleIconProps = () => (
     />
 );
 
+class Result extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            found: false,
+            match_info: {},
+            results: 20
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>RESULTS PAGE</h1>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            match_info: null,
-            fetching: true,
-            val: 1
+            match_info: null
         };
     }
 
-    componentDidMount() {
-        // Axios.get('/riot/api/MarkandSweep')
-        //     .then(response => {
-        //         this.setState({
-        //             match_info: response.data.match_info,
-        //             fetching: false
-        //         });
-        //         console.log(response)
-        //     })
-        //     .catch(err => {
-        //         this.setState({
-        //             match_info: err,
-        //             fetching: false,
-        //             val: 1
-        //         })
-        //     })
-    }
-
     render() {
-        return (
-            <div className="App">
+        if (this.state.match_info === null) {
+            return (
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1>LoL<span>FY</span></h1>
+                        <p>Mini LoL Stat Application. Powered by React and NodeJS.</p>
+                    </div>
 
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1>LoL<span>FY</span></h1>
-                    <p>Mini LoL Stat Application. Powered by React and NodeJS.</p>
+                    <SummonerSearchBox/>
+
                 </div>
-
-                <InputExampleIconProps/>
-
-            </div>
-        );
+            );
+        } else {
+            return (
+                <Result/>
+            )
+        }
     }
 }
 
