fc8f153b6ca7b063f48b7e8560dc10390827a81e
Mark renzo Santiago <ms103804@gmail.com>
2017-11-19 20:06:28 -0800

    initialize routing

---
+++ server/routes/riot/controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let controller = {};
+
+/**
+ * Grabs the Summoner data for their match history and returns it to the user formatted nicely
+ *
+ * @param req
+ * @param res
+ */
+controller.getSummonerInfo = (req, res) => {
+
+};
+
+module.exports = controller;
