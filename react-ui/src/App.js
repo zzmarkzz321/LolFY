6085b176db7e29a2507de703ece4a5d73d245dae
Mark renzo Santiago <ms103804@gmail.com>
2017-11-19 20:09:06 -0800

    change import location

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import Axios from 'axios';
-import {Navigation} from "./Components/standard-nav";
+import {Navigation} from "./components/standard-nav";
 
 class App extends Component {
     constructor(props) {
@@ -12,39 +12,9 @@ class App extends Component {
             fetching: true,
             val: 1
         };
-
-        this.changeVal = this.changeVal.bind(this);
-        this.changeMessage = this.changeMessage.bind(this);
     }
 
     componentDidMount() {
-        Axios.get('/api?val=' + this.state.val)
-            .then(response => {
-                this.setState({
-                    message: response.data.message,
-                    fetching: false,
-                    val: 1
-                })
-            })
-            .catch(err => {
-                this.setState({
-                    message: err,
-                    fetching: false,
-                    val: 1
-                })
-            })
-    }
-
-    changeVal() {
-        this.setState({
-            val: (this.state.val < 3) ? this.state.val + 1 : 1
-        });
-        console.log(this.state.val);
-        this.changeMessage();
-    }
-
-    changeMessage() {
-        console.log('changeMessage');
         Axios.get('/api?val=' + this.state.val)
             .then(response => {
                 this.setState({
@@ -55,11 +25,11 @@ class App extends Component {
             .catch(err => {
                 this.setState({
                     message: err,
-                    fetching: true
+                    fetching: false,
+                    val: 1
                 })
             })
     }
-
 
     render() {
         return (
