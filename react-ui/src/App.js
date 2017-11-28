4a35bdb6fce01de84382ef2acc04dff98de997d7
Mark renzo Santiago <ms103804@gmail.com>
2017-11-27 19:20:21 -0800

    fixed search input and refactored components

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import Axios from 'axios';
+import logo from './logo.svg';
 
-import { Result, Home } from './components/'
+import { Result, SearchBox } from './components/'
+import Axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             match_info: false,
-            summoner_name: 'default',
+            summoner_name: '',
             outcome: '',
             gameLength: '',
             summonerName: '',
@@ -22,20 +23,19 @@ class App extends Component {
             creepScorePerMinute: ''
         };
 
-        this._onSubmit = this._onSubmit.bind(this);
+        this._handleSummonerNameChange = this._handleSummonerNameChange.bind(this);
+        this._handleSummonerSearchSubmit = this._handleSummonerSearchSubmit.bind(this);
         this._resetState = this._resetState.bind(this);
     }
 
-    _onChange(event) {
+    _handleSummonerNameChange(summoner_name) {
         this.setState({
-            summoner_name: event.target.summoner_name
-        });
+            summoner_name
+        })
     }
 
-    _onSubmit(event) {
-        console.log(event.target.value);
-        event.preventDefault();
-        Axios.get('/riot/api/mock/Mark%20and%20Sweep')
+    _handleSummonerSearchSubmit() {
+        Axios.get('/riot/api/mock/' + this.state.summoner_name)
             .then((result) => {
 
                 console.log('****');
@@ -66,24 +66,22 @@ class App extends Component {
     _resetState() {
         this.setState({
             match_info: false,
-            summoner_name: '',
-            outcome: '',
-            gameLength: '',
-            summonerName: '',
-            summonerSpells: [],
-            champion: '',
-            kda: '',
-            items: [],
-            level: '',
-            creepScore: '',
-            creepScorePerMinute: ''
+            summoner_name: ''
         });
     }
 
     render() {
         if (!this.state.match_info) {
             return (
-                <Home _onSubmit={this._onSubmit} _onChange={this._onChange}/>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1>LoL<span>FY</span></h1>
+                        <p>Mini LoL Stat Application. Powered by React and NodeJS.</p>
+                    </div>
+
+                    <SearchBox summoner_name={this.state.summoner_name} _handleSummonerSearchSubmit={this._handleSummonerSearchSubmit} _handleSummonerNameChange={this._handleSummonerNameChange}/>
+                </div>
             );
         } else {
             return (
